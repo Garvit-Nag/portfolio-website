5f63cbe088f02f61d517e22a5781664a7b62ddcd
Garvit <garvitcpp@gmail.com>
2025-05-01 23:09:13 +0530

    refactor: remove Particles background component from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 // src/app/page.tsx
 "use client";
 
-import { Particles } from "@/components/magicui/particles";
+
 import AboutSection from "@/components/ui/AboutSection";
 import ContactSection from "@/components/ui/ContactSection";
 import HeroSection from "@/components/ui/HeroSection";
@@ -10,16 +10,7 @@ import ProjectsSection from "@/components/ui/projects/ProjectsSection";
 export default function Home() {
   return (
     <div className="relative min-h-screen w-full overflow-hidden">
-      {/* Particles Background */}
-      <div className="absolute inset-0 z-0">
-        <Particles
-          className="absolute inset-0"
-          quantity={200}
-          staticity={30}
-          color="#ffffff"
-          ease={30}
-        />
-      </div>
+
       
       {/* Navbar */}
       <Navbar />
