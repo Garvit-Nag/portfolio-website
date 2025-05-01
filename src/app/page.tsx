61682ceb3b12dc76e981a006e1cfe9bf4f11b468
Garvit <garvitcpp@gmail.com>
2025-05-01 22:55:12 +0530

    feat: add Particles background component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 // src/app/page.tsx
 "use client";
 
-
+import { Particles } from "@/components/magicui/particles";
 import AboutSection from "@/components/ui/AboutSection";
 import ContactSection from "@/components/ui/ContactSection";
 import HeroSection from "@/components/ui/HeroSection";
@@ -11,7 +11,15 @@ export default function Home() {
   return (
     <div className="relative min-h-screen w-full overflow-hidden">
       {/* Particles Background */}
-      
+      <div className="absolute inset-0 z-0">
+        <Particles
+          className="absolute inset-0"
+          quantity={200}
+          staticity={30}
+          color="#ffffff"
+          ease={30}
+        />
+      </div>
       
       {/* Navbar */}
       <Navbar />
