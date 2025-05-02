b098997c0e882bda4b359082820f1581bb59a288
Garvit <garvitcpp@gmail.com>
2025-05-02 20:28:15 +0530

    refactor: remove StaticParticles component and adjust background rendering logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import AboutSection from "@/components/ui/AboutSection";
 import ContactSection from "@/components/ui/contact/ContactSection";
 import HeroSection from "@/components/ui/HeroSection/HeroSection";
 import Navbar from "@/components/ui/Navbar";
-import { StaticParticles } from "@/components/ui/StaticParticles";
 import ProjectsSection from "@/components/ui/projects/ProjectsSection";
 import { useEffect, useState } from "react";
 
@@ -15,14 +14,13 @@ export default function Home() {
   useEffect(() => {
     if (typeof window !== "undefined") {
       const checkMobile = () => {
-        setIsMobile(window.innerWidth < 768);
+        setIsMobile(window.innerWidth < 768); 
       };
-
-      // Initial check
+      
       checkMobile();
-
+      
       window.addEventListener("resize", checkMobile);
-
+      
       return () => window.removeEventListener("resize", checkMobile);
     }
   }, []);
@@ -40,13 +38,10 @@ export default function Home() {
             ease={30}
           />
         ) : (
-
-          <StaticParticles
-            className="absolute inset-0"
-            quantity={30}
-            color="#ffffff"
-            size={0.4}
-          />
+          <div 
+            className="absolute inset-0" 
+            style={{ backgroundColor: "rgba(1, 6, 18, 0.97)" }}
+          ></div>
         )}
       </div>
 
